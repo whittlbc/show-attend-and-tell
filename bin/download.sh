91795619d69fbd26890ca44a8963f97bf1a654e3
Ben Whittle <benwhittle31@gmail.com>
2017-10-19 01:11:59 -0700

    moving download.sh to bin/

--- bin/download.sh
+++ bin/download.sh
@@ -4,7 +4,10 @@ wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip -P image/
 wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip -P image/
 
 unzip data/captions_train-val2014.zip -d data/
+rm data/captions_train-val2014.zip
+
 unzip image/train2014.zip -d image/
-rm image/train2014.zip 
+rm image/train2014.zip
+
 unzip image/val2014.zip -d image/ 
-rm image/val2014.zip 
+rm image/val2014.zip
